c1f3bb5e2ff0ff6fb67408beb902a3c2e6a49219
Ashraful Islam <theashrafislam@gmail.com>
2024-12-07 00:51:17 +0600

    learn tuple-type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,6 @@
+// mixed data - key, value;
+var user;
+user = [10, '10'];
+user.push(101, 'riya');
+console.log(user);
+console.log(user[1]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mixed data - key, value;
+
+let user: [number, string];
+user  = [10, '10'];
+user.push(101, 'riya');
+
+console.log(user);
+console.log(user[1]);
