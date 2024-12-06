3f05d2697a5572aa6d2f93892ceb392c9e2af82d
Ashraful Islam <theashrafislam@gmail.com>
2024-12-07 00:28:03 +0600

    learn array-type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,10 @@
+// let userName = ['anis', 'rabu', 'pinky'];
+var userName;
+// let userName: Array<string>;
+var multipleTypes;
+userName = ['anis', 'borsha', 'rabu', 'panky'];
+userName.sort();
+multipleTypes = ['10', 10];
+// console.log(userName[0]);
+console.log(userName);
+// export{userName}
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,15 @@
+// let userName = ['anis', 'rabu', 'pinky'];
+
+let userName: string[];
+// let userName: Array<string>;
+
+let multipleTypes: (string | number)[];
+
+userName = ['anis', 'borsha', 'rabu', 'panky'];
+userName.sort();
+multipleTypes = ['10', 10]
+
+// console.log(userName[0]);
+console.log(userName);
+
+// export{userName}
